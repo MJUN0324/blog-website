660b5c816e37c43e1df2c2e23edc2e43f7fe4f41
Jun <jun0324p@gmail.com>
2023-12-19 19:48:51 +0800

    Add tailwind css

---
+++ src/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header id="main-header">
+      <div id="title">
+        <h1 className="text-3xl font-bold underline">Physics Web</h1>
+      </div>
+      <nav></nav>
+    </header>
+  );
+}
---
+++ src/components/buttons/Button.tsx
@@ -0,0 +1,5 @@
+import { ReactPortal } from "react";
+
+export default function Button(children: ReactPortal) {
+  return <button>{children}</button>;
+}
